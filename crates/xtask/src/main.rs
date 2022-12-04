e25200c0d668971bf88dca0f8a67c877a5983b89
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-12-04 22:27:56 +0800

    Got rid of the warnings in xtask

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 
 fn main() {
-    match Cmd::parse() {}
+    let cmd = Cmd::parse();
+
+    todo!("Run {cmd:?}");
 }
 
 #[derive(Parser, Debug)]
 #[command(author, version)]
-enum Cmd {
-    // TODO: add some arguments
-}
+enum Cmd {}
